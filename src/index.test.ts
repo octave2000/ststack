79e8e42ec9ed3847bbe3a4c6b996b10ec18d69b1
octave2000 <nkurunzizaheuruex@gmail.com>
2024-09-13 17:10:24 +0200

    dev branch

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,5 @@
 // sum.test.js
 import { expect, test } from "vitest";
-import { sum } from "./index";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(1).toBe(1);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-// sum.js
-export function sum(a: number, b: number) {
-  return a + b + 1 + 1;
-}
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program.name("st-stack").description("package.json").version("0.0.5");
+
+program.command("hi").action(async () => {
+  console.log("done");
+});
