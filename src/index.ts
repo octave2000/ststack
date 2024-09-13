65d7633f6adac10673ab8549b6078edf02da3613
octave2000 <nkurunzizaheuruex@gmail.com>
2024-09-13 18:08:24 +0200

    starter kit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import inquirer from "inquirer";
 
 const program = new Command();
 
-program.name("st-stack").description("package.json").version("0.0.5");
+program
+  .name("st-stack")
+  .description("package.json")
+  .version("0.0.5")
+  .action(async () => {
+    const packages = await inquirer.prompt({
+      name: "package",
+      type: "list",
+      message: "choose prefered package",
+      choices: ["yarn", "npm"],
+    });
 
-program.command("hi").action(async () => {
-  console.log("done");
-});
+    if (packages.package === "yarn") {
+      const framework = inquirer.prompt({
+        name: "framework",
+        type: "list",
+        message: "choose prefered package",
+        choices: ["sadas", "nsasdsd"],
+      });
+    } else {
+      console.log(packages.package);
+    }
+  });
+
+program.parse();
